1bb57f4005b4c91c9f6401f5b45f65d0bac3c0d2
mmmari4ka <109090925+mmmari4ka@users.noreply.github.com>
2022-09-06 22:38:14 +0200

    Removed the forecast 👋

--- src/app.js
+++ src/app.js
@@ -74,45 +74,6 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForecast(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
-              <div class="col-2">
-                <div class="weather-forecast-date">${formatDay(
-                  forecastDay.dt
-                )}</div>
-                <img
-                  src="https://openweathermap.org/img/wn/${
-                    forecastDay.weather[0].icon
-                  }@2x.png"
-                  alt="sunny"
-                  width="46"
-                />
-                <br />
-                <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max"> ${Math.round(
-                    forecastDay.temp.max
-                  )}° </span>
-                  <span class="weather-forecast-temperature-min"> ${Math.round(
-                    forecastDay.temp.min
-                  )}° </span>
-                </div>
-              </div>
-            </div>`;
-    }
-  });
-
-  forecastHTML = forecastHTML + `<\div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
